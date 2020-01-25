8a84531d8e30e6bfc84ed0a55920fcd20786f0ce
Ali Asadpoor <virtualmatador@yahoo.com>
2020-01-25 07:13:53 +0000

    Use stereogram with STL random generator

--- src/game.cpp
+++ src/game.cpp
@@ -49,7 +49,7 @@ void main::Game::Initial()
     border_ = cell_ / 3;
     margin_[0] = (height_ - cell_ * data_.c_y_ - border_ * 2) / 2;
     margin_[1] = (width_ - cell_ * data_.c_x_ - border_ * 2) / 2;
-    pattern_target_ = stereogram::CreateRandomPattern<4>(column_, order_rgba_);
+    pattern_target_ = stereogram::CreatePattern<4>(column_, order_rgba_);
     pattern_current_.resize(pattern_target_.size());
     pattern_base_.resize(pattern_target_.size());
 }
@@ -64,7 +64,7 @@ void main::Game::Step(__uint32_t* pixels)
     if (++pattern_frame_ == FR)
     {
         std::swap(pattern_base_, pattern_target_);
-        pattern_target_ = stereogram::CreateRandomPattern<4>(column_, order_rgba_);
+        pattern_target_ = stereogram::CreatePattern<4>(column_, order_rgba_);
         pattern_frame_ = 0;
     }
     for (int i = 0; i < pattern_current_.size(); ++i)
